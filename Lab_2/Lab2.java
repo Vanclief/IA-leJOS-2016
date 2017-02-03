f77fd5ab2aebab24d74d37870cdd706dc3e9e310
Gustavo Gutiérrez <squgus@gmail.com>
2017-02-03 03:42:26 -0600

    HitWall and Explore behaviors done! RunFromSound behavior needs to be polished

--- Lab_2/Lab2.java
+++ Lab_2/Lab2.java
@@ -1,7 +1,10 @@
+import java.util.Random;
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.Sound;
+import lejos.util.Delay;
 import lejos.nxt.LightSensor;
+import lejos.nxt.TouchSensor;
 import lejos.nxt.UltrasonicSensor;
 import lejos.nxt.SoundSensor;
 import lejos.nxt.SensorPort;
@@ -13,36 +16,166 @@ import lejos.robotics.subsumption.Behavior;
 import lejos.util.PilotProps;
 
 
-public class Sub {
+public class Lab2 {
 
   public static void main (String[] aArg) throws Exception {
 
     PilotProps pp = new PilotProps();
     pp.loadPersistentValues();
 
+    final Random random = new Random();
+
     final int soundThreshold = 50;
 
-    float wheelDiameter = Float.parseFloat(pp.getProperty(PilotProps.KEY_WHEELDIAMETER, "4.96"));
-    float trackWidth = Float.parseFloat(pp.getProperty(PilotProps.KEY_TRACKWIDTH, "13.0"));
+    // Motors
+    final RegulatedMotor leftMotor = PilotProps.getMotor(pp.getProperty(PilotProps.KEY_LEFTMOTOR, "C"));
+    final RegulatedMotor rightMotor = PilotProps.getMotor(pp.getProperty(PilotProps.KEY_RIGHTMOTOR, "A"));
+    // boolean reverse = Boolean.parseBoolean(pp.getProperty(PilotProps.KEY_REVERSE,"false"));
+    // Wheel calibration values
+    float wheelDiameter = Float.parseFloat(pp.getProperty(PilotProps.KEY_WHEELDIAMETER, "2"));
+    float trackWidth = Float.parseFloat(pp.getProperty(PilotProps.KEY_TRACKWIDTH, "6.25"));
 
-    RegulatedMotor leftMotor = PilotProps.getMotor(pp.getProperty(PilotProps.KEY_LEFTMOTOR, "B"));
-    RegulatedMotor rightMotor = PilotProps.getMotor(pp.getProperty(PilotProps.KEY_RIGHTMOTOR, "A"));
-    boolean reverse = Boolean.parseBoolean(pp.getProperty(PilotProps.KEY_REVERSE,"false"));
+    // Pilot
+    final DifferentialPilot pilot = new DifferentialPilot(wheelDiameter, trackWidth, leftMotor, rightMotor);
 
+    // Light sensor
+    final LightSensor light = new LightSensor(SensorPort.S1, false);
 
-    //reverse
-    final RotateMoveController pilot = new DifferentialPilot(wheelDiameter, trackWidth, leftMotor, rightMotor, reverse);
-    // final LightSensor light = new LightSensor(SensorPort.S1);
-    UltrasonicSensor ultrasonicSensor = new UltrasonicSensor(SensorPort.S1);
+    // Ultrasonic sensor
+    // final UltrasonicSensor ultrasonicSensor = new UltrasonicSensor(SensorPort.S1);
+
+    // Sound sensor
     final SoundSensor sound = new SoundSensor(SensorPort.S4);
 
-    pilot.setRotateSpeed(180);
-    /**
-     * this behavior wants to take control when the light sensor sees the line
-     */
+    // Touch sensors
+    final TouchSensor[] touch = new TouchSensor[2];
+    touch[0] = new TouchSensor(SensorPort.S2);
+    touch[1] = new TouchSensor(SensorPort.S3);
 
-    // DRIVE --------------------------------------------------
-    Behavior Drive = new Behavior(){
+    pilot.setRotateSpeed(180);
+
+    // LEAST IMPORTANT BEHAVIOR
+    // EXPLORE ------------------------------------------------------------------------------------
+    Behavior Explore = new Behavior() {
+      private boolean suppressed = false;
+      private int[] lightIntensities = new int[3];
+
+      public boolean takeControl() {
+        return true;
+      }
+
+      public void action() {
+        LCD.drawString("EXPLORING", 0, 0);
+        // LCD.drawString("light: " + light.readValue(), 0, 1);
+
+        lightIntensities[0] = light.readValue();
+        pilot.rotate(45);
+        lightIntensities[1] = light.readValue();
+        pilot.rotate(-90);
+        lightIntensities[2] = light.readValue();
+
+        if (lightIntensities[0] <= lightIntensities[1] && lightIntensities[0] <= lightIntensities[2]) {
+          LCD.drawString("CENTER", 0, 1);
+          pilot.rotate(45);
+        } else {
+          if (lightIntensities[1] <= lightIntensities[0] && lightIntensities[1] <= lightIntensities[2]) {
+            LCD.drawString("LEFT", 0, 1);
+            pilot.rotate(90);
+          } else {
+            LCD.drawString("RIGHT", 0, 1);
+          }
+        }
+
+        pilot.travel(5, true);
+        while(pilot.isMoving() && !suppressed) {
+          Thread.yield();
+        }
+        Delay.msDelay(1000);
+        LCD.clear();
+      }
+
+      public void suppress() {
+        suppressed = true;
+      }
+    };
+
+    // SECOND MOST IMPORTANT BEHAVIOR
+    // RUN FROM SOUND -----------------------------------------------------------------------------
+    Behavior RunFromSound = new Behavior(){
+      private boolean suppress = false;
+
+      // This behavior should be triggered if the sound value exceds the soundThreshold
+      public boolean takeControl() {
+        return sound.readValue() > soundThreshold;
+      }
+
+      // If this behavior stops, stop moving
+      public void suppress() {
+        suppress = true;
+      }
+
+      public void action() {
+
+        LCD.drawString("SCARED", 0, 0);
+        LCD.drawString("Im scared of", 0, 1);
+        LCD.drawString("sounds. ", 0, 2);
+
+        // While the behavior is still running, go backward and try going any of both sides
+        while (!suppress) {
+
+          // Beep since its a little ~bitch~ mousey mousey
+          Sound.beepSequence();
+
+          // Go back
+          pilot.travel(-5 - random.nextInt(5));
+
+          // Get a random direction
+          int angle = 60 + random.nextInt(60);
+          int side = (Math.random() > 0.5 ? -1 : 1);
+
+          // Rotate
+          pilot.rotate(-side * angle);
+
+          while (!suppress && pilot.isMoving())
+            Thread.yield();
+        }
+        LCD.clear();
+      }
+    };
+
+    // MOST IMPORTANT BEHAVIOR
+    // HIT WALL -----------------------------------------------------------------------------------
+    Behavior HitWall = new Behavior() {
+      private boolean suppressed = false;
+      private int degrees;
+
+      public boolean takeControl() {
+        return touch[0].isPressed() || touch[1].isPressed();
+      }
+
+      public void action() {
+        LCD.drawString("I HIT A WALL!", 0, 0);
+        degrees = random.nextInt(90) + 135;
+
+        suppressed = false;
+        pilot.travel(-7.5);
+        pilot.rotate(degrees);
+        pilot.travel(10, true);
+
+        while (pilot.isMoving() && !suppressed) {
+          Thread.yield();
+        }
+        LCD.clear();
+      }
+
+      public void suppress() {
+        suppressed = true;
+      }
+    };
+
+    // DEPRECATED
+    // DRIVE --------------------------------------------------------------------------------------
+    Behavior Drive = new Behavior() {
       public boolean takeControl() {
         return sound.readValue() <= soundThreshold;
       }
@@ -61,7 +194,8 @@ public class Sub {
       }
     };
 
-    // MOVE BACK --------------------------------------------------
+    // DEPRECATED
+    // MOVE BACK ----------------------------------------------------------------------------------
     Behavior MoveBack = new Behavior(){
       private boolean suppress = false;
 
@@ -87,52 +221,35 @@ public class Sub {
       }
     };
 
-    // RUN AWAY FROM SOUND --------------------------------------------------
-    Behavior RunFromSound = new Behavior() {
+    // DEPRECATED
+    // RUN AWAY FROM SOUND ------------------------------------------------------------------------
+    Behavior RunAway = new Behavior(){
+      private boolean suppress = false;
 
-      private boolean supress = false;
-
-      // This behavior should be triggered if the sound value exceds the soundThreshold
       public boolean takeControl() {
         return sound.readValue() > soundThreshold;
       }
 
-      // If this behavior stops, stop moving
-      public void supress() {
+      public void suppress() {
         suppress = true;
       }
 
       public void action() {
-
         LCD.drawString("Status: SCARED", 0, 0);
-        LCD.drawString("Im scared of", 1, 0);
-        LCD.drawString("sounds. ", 2, 0);
+        LCD.drawString("Im scared of", 0, 1);
+        LCD.drawString("sounds. ", 0, 2);
 
-        // While the behavior is still running, go backward and try going any of both sides
         while (!suppress) {
-
-          // Beep since its a little ~bitch~ mousey mousey
-          Sound.beepSequence();
-
-          // Go back
-          pilot.travel(-5 - rand.nextInt(5));
-
-          // Get a random direction
-          int angle = 60 + rand.nextInt(60);
-          int side = (Math.random() > 0.5 ? -1 : 1);
-
-          // Rotate
-          pilot.rotate(-side * angle);
-
+          pilot.backward();
           while (!suppress && pilot.isMoving())
             Thread.yield();
         }
-
+        pilot.stop();
+        suppress = false;
       }
-
     };
 
-    Behavior[] behaviors = {MoveBack, Drive, RunFromSound};
+    Behavior[] behaviors = {Explore, RunFromSound, HitWall};
     Button.waitForAnyPress();
     (new Arbitrator(behaviors)).start();
   }
